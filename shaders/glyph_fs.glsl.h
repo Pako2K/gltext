3f364e8903e47fc8cb2a88231815c6cbb6b4f0da
Pako2K <41144260+Pako2K@users.noreply.github.com>
2018-08-19 11:18:29 +0200

    Add files via upload

--- shaders/glyph_fs.glsl.h
+++ shaders/glyph_fs.glsl.h
@@ -1,17 +1,21 @@
 const char __FRAG_SHADER_SRC__ [] =
 " \
+ \n\
 #version 400 core \n\
-\
-in vec2 vTexCoords; \
-\
-out vec4 fColor; \
-\
-uniform sampler2D text; \
-uniform vec3 uTextColor; \
-\
-void main() { \
-  vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, vTexCoords).r); \
-\
-  fColor = vec4(uTextColor, 1.0) * sampled;  \
-} \
+ \n\
+in vec2 vTexCoords; \n\
+ \n\
+out vec4 fColor; \n\
+ \n\
+uniform sampler2D text; \n\
+uniform vec3 uTextColor; \n\
+ \n\
+ \n\
+void main() { \n\
+  vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, vTexCoords).r); \n\
+ \n\
+	 \n\
+  fColor = vec4(uTextColor, 1.0) * sampled; \n\
+} \n\
+ \n\
 ";
--- shaders/glyph_vs.glsl.h
+++ shaders/glyph_vs.glsl.h
@@ -1,16 +1,18 @@
 const char __VERT_SHADER_SRC__ [] =
 " \
+ \n\
 #version 400 core \n\
-\
-in ivec2 inPosition; \
-in vec2 inTexCoord; \
-\
-out vec2 vTexCoords; \
-\
-uniform mat4 uProjection; \
-\
-void main() { \
-    gl_Position = uProjection * vec4(inPosition, 1.0, 1.0); \
-    vTexCoords = inTexCoord; \
-} \
+ \n\
+in ivec2 inPosition; \n\
+in vec2 inTexCoord; \n\
+ \n\
+out vec2 vTexCoords; \n\
+ \n\
+uniform mat4 uProjection; \n\
+ \n\
+void main() { \n\
+  gl_Position = uProjection * vec4(inPosition, 1.0, 1.0); \n\
+  vTexCoords = inTexCoord; \n\
+} \n\
+ \n\
 ";
